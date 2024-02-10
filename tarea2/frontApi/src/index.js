496295012f466c43872571b8692bb99229934f0d
Kruskalt <codigorap13@gmail.com>
2024-02-10 11:08:05 -0300

    se listan los equipos y se agrega el evento click con una funcion pasada como parametro

--- tarea2/frontApi/src/index.js
+++ tarea2/frontApi/src/index.js
@@ -1,9 +1,15 @@
 import { pedirEquipos } from "./servicios/servicios.js";
 import { listarEquipos } from "./ui/home.js";
 
+function irApaginaDelEquipo(nombre) {
+    window.location.href = `/equipo?name=${nombre}`;
+}
 
 async function inicializar() {
 
-listarEquipos(await pedirEquipos());
+listarEquipos(await pedirEquipos(),irApaginaDelEquipo);
 }
 inicializar();
+
+
+
--- tarea2/frontApi/src/ui/home.js
+++ tarea2/frontApi/src/ui/home.js
@@ -1,49 +1,25 @@
-
-export async function listarEquipos(equipos) {
+export async function listarEquipos(equipos, equipoSeleccionadoCallback = () => { }) {
     equipos.forEach(equipo => {
-        console.log(equipo.crestUrl)
-        agregarEscudoAlMain(equipo.crestUrl,equipo.name);
-    });
-}
-
-export function agregarEscudoAlMain(imagenEscudo,name) {
-        // Crear elementos y configurar sus atributos y contenido
         const colDiv = document.createElement('div');
         colDiv.classList.add('col');
-      
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('card', 'main__card');
         cardDiv.style.display = 'inline-block';
-      
+
         const img = document.createElement('img');
-        img.src = imagenEscudo;
-        
+        img.src = equipo.crestUrl;
+
         img.alt = '...';
-      
+
         const cardBodyDiv = document.createElement('div');
         cardBodyDiv.classList.add('card-body');
-      
-        // const title = document.createElement('h5');
-        // title.classList.add('card-title');
-        // title.textContent = `#${idPokemon}`;
-      
-        // Agregar elementos al árbol DOM
-        // cardBodyDiv.appendChild(title);
         cardDiv.appendChild(img);
         cardDiv.appendChild(cardBodyDiv);
         colDiv.appendChild(cardDiv);
-        colDiv.addEventListener('click', function () {
-           window.location.href= "/paginaEquipo.html"
-           nombre(name)
-          });
+        colDiv.onclick = () => equipoSeleccionadoCallback(equipo.name);
+
         const rowElement = document.querySelector('.row');
         rowElement.appendChild(colDiv);
-}
 
-
-function nombre(p) {
-    let body= document.querySelector("body");
-    let titulo = document.createElement("h1")
-    titulo.textContent=p
-    body.appendChild(titulo)
+    });
 }
