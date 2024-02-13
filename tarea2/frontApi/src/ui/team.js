2fecc54deef5eb5e031dc5693aac7787e9206d5f
Kruskalt <codigorap13@gmail.com>
2024-02-13 14:04:35 -0300

    se ve la informacion que faltaba del equipo en particular y agrego un test de esa funcion en jest

--- tarea2/frontApi/src/ui/team.js
+++ tarea2/frontApi/src/ui/team.js
@@ -1,22 +1,27 @@
 export function listarInformacionDelEquipo(equipo) {
-    const $imgEscudo = document.querySelector(".escudo-equipo");
-    const urlImagen= equipo.crestUrl;
-    console.log("soy",$imgEscudo)
-    
-    if (urlImagen.startsWith("https://")) {
-        $imgEscudo.src= `${equipo.crestUrl}`
-    }else{
-        $imgEscudo.src= `/${equipo.crestUrl}`
-    }
-   
-
-    const $titulo = document.querySelector(".titulo")
-    $titulo.textContent = `${equipo.name}`
+    console.log(equipo)
+  const $imgEscudo = document.querySelector(".escudo-equipo");
+  const urlImagen = equipo.crestUrl;
+  if (urlImagen !== undefined && urlImagen.startsWith("https://") ) {
+    $imgEscudo.src = `${equipo.crestUrl}`;
+  } else {
+    $imgEscudo.src = `/${equipo.crestUrl}`;
+  }
+  const $titulo = document.querySelector(".titulo");
+  $titulo.textContent = `${equipo.name}`;
+  document.querySelector("#fundacion").textContent = `${equipo.founded}`;
+  document.querySelector("#direccion").textContent = `${equipo.address}`;
+  document.querySelector("#telefono").textContent = `${equipo.phone}`;
+  document.querySelector("#website").href = `${equipo.website}`;
+  document.querySelector("#website").textContent = `${equipo.website}`;
+  document.querySelector("#email").textContent = `${equipo.email}`;
+  document.querySelector("#email").href = `mailto:${equipo.email}`;
+  document.querySelector("#estadio").textContent = `${equipo.venue}`;
 }
 
 export function obtenerIdUrl() {
     var urlParams = new URLSearchParams(window.location.search);
     var id = urlParams.get('id');
-    
+
     return id;
 }
